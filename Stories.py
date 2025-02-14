05ace0efc25fa43cd09b28958c6ca36a199eb699
legolandm <31925979+legolandm@users.noreply.github.com>
2025-02-14 12:21:05 -0500

    Update Stories.py

--- Stories.py
+++ Stories.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "Enter your API Key here"
+API_KEY = "AIzaSyASCsbRmNLwWJ0bgu5Os18LLVjDOhu4sLo"
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
